0f06705832d030a617f4fb3e4d15229cdf59b9a6
ratish11 <ratishjha47@gmail.com>
2023-11-07 17:15:21 -0500

    restructured and added backend code

---
+++ Backend/flask.py
@@ -0,0 +1,20 @@
+import numpy as np
+from flask import Flask, request, jsonify
+from sklearn.metrics import accuracy_score, f1_score, classification_report
+import  sys, os, re, logging
+from transformers import pipeline
+
+app = Flask(__name__)# Load the model
+
+fill = pipeline('fill-mask', model=model_name, tokenizer=tokenizer)
+
+@app.route('/api',methods=['POST'])
+def predict():
+    # Get the data from the POST request.
+    data = request.get_json(force=True)    # Make prediction using model loaded from disk as per the data.
+    prediction = model.predict([[np.array(data['exp'])]])    # Take the first value of prediction
+    resp = fill(test_seq)[0]['token_str'] 
+    return jsonify(resp)
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
