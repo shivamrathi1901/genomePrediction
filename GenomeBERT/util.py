5026a21a257c0a707a09ac0da8c949a76e48356d
ratish11 <ratishjha47@gmail.com>
2023-11-18 20:34:24 -0500

    val = 6%

--- GenomeBERT/util.py
+++ GenomeBERT/util.py
@@ -12,8 +12,8 @@ def dataload(file):
 
 def split_dataset(rawdata):
     from sklearn.model_selection import train_test_split
-    train_set, test_val_set = train_test_split(rawdata, test_size=0.3, random_state=42)
-    val_set, test_set = train_test_split(test_val_set, test_size=0.5, random_state=42)
+    train_set, test_val_set = train_test_split(rawdata, test_size=0.2, random_state=42)
+    val_set, test_set = train_test_split(test_val_set, test_size=0.7, random_state=42)
     return train_set, val_set, test_set
 
 
