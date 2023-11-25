b4b75d760c330a4833041f2f6cd87e484c1899b2
Soumya130498 <113308735+Soumya130498@users.noreply.github.com>
2023-11-24 19:08:58 -0500

    Create test.py

---
+++ GenomeBERT/test.py
@@ -0,0 +1,69 @@
+
+from transformers import AutoTokenizer, AutoModel, pipeline
+import random, logging, sys
+from sklearn.metrics import classification_report
+import pandas as pd
+
+logging.basicConfig(filename="log/{}_{}.log".format(sys.argv[1], 'test'),
+                    format="%(asctime)s [%(levelname)s]: %(message)s",
+                    filemode='w')
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+def run_test(sequences, model, tokenizer):
+    fill = pipeline('fill-mask', model=model, tokenizer=tokenizer)
+
+    result = []
+    expected = []
+
+    for sequence in sequences:
+        idx = random.randint(0, len(sequence) - 1)
+        test_seq = sequence[:idx] + fill.tokenizer.mask_token + sequence[idx + 1:]
+        expected.append(sequence[idx: idx + 1])
+
+        # Directly access the model outputs without modifying the tuple
+        fill_output = fill(test_seq)
+        #resp = fill_output[0]['token_str']
+        result.append(fill_output)
+
+    return result, expected
+
+def chunk_sequences(file_path, chunk_size, model, tokenizer):
+    reader = pd.read_csv(file_path, chunksize=chunk_size)
+
+    for chunk in reader:
+        sequences = chunk['Sequence'].tolist()
+        results, expected = run_test(sequences, model, tokenizer)
+        # Process the results and expected values as needed
+        for res, exp in zip(results, expected):
+            print(f"Expected: {exp}  Response: {res}")
+            if(len(expected)%100==0):
+                logger.info("processing {}".format(len(expected)))
+            print(classification_report(results,expected))
+
+if __name__ == '__main__':
+    csv_file_path = "./test.csv"
+    model_name = "GenomeBERT"
+    tokenizer = AutoTokenizer.from_pretrained("zhihan1996/DNABERT-2-117M", trust_remote_code=True)
+    model = AutoModel.from_pretrained(model_name, trust_remote_code=True)
+    chunk_sequences(csv_file_path, chunk_size=2, model=model, tokenizer=tokenizer)
+
+# import pandas as pd
+# from transformers import AutoTokenizer, AutoModel, pipeline
+# import transformers
+# csv_file_path = "test.csv"
+# model_name = "GenomeBERT"
+# expected = []
+# #tokenizer = AutoTokenizer.from_pretrained("zhihan1996/DNABERT-2-117M", trust_remote_code=True)
+# #model = AutoModel.from_pretrained(model_name, local_files_only=True, trust_remote_code=True)
+# sequence = "ATGATTGTCGGCATTCTCACCACGCTGGCTACGCTGGCCACACTCGCAGCTAGTGTGCCTCTAGAGGAGCGGCAAGCTTGCTCAAGCGTCTGGGGCCAATGTGGTGGCCAGAATTGGTCGGGTCCGACTTGCTGTGCTTCCGGAAGCACATGCGTCTACTCCAACGACTATTACTCCCAGTGTCTTCCCGGCGCTGCAAGCTCAAGCTCGTCCACGCGCGCCGCGTCGACGACTTCTCGAGTATCCCCCACAACATCCCGGTCGAGCTCCGCGACGCCTCCACCTGGTTCTACTACTACCAGAGTACCTCCAGTCGGATCGGGAACCGCTACGTATTCAGGCAACCCTTTTGTTGGGGTCACTCCTTGGGCCAATGCATATTACGCCTCTGAAGTTAGCAGCCTCGCTATTCCTAGCTTGACTGGAGCCATGGCCACTGCTGCAGCAGCTGTCGCAAAGGTTCCCTCTTTTATGTGGCTAGATACTCTTGACAAGACCCCTCTCATGGAGCAAACCTTGGCCGACATCCGCACCGCCAACAAGAATGGCGGTAACTATGCCGGACAGTTTGTGGTGTATGACTTGCCGGATCGCGATTGCGCTGCCCTTGCCTCGAATGGCGAATACTCTATTGCCGATGGTGGCGTCGCCAAATATAAGAACTATATCGACACCATTCGTCAAATTGTCGTGGAATATTCCGATATCCGGACCCTCCTGGTTATTGAGCCTGACTCTCTTGCCAACCTGGTGACCAACCTCGGTACTCCAAAGTGTGCCAATGCTCAGTCAGCCTACCTTGAGTGCATCAACTACGCCGTCACACAGCTGAACCTTCCAAATGTTGCGATGTATTTGGACGCTGGCCATGCAGGATGGCTTGGCTGGCCGGCAAACCAAGACCCGGCCGCTCAGCTATTTGCAAATGTTTACAAGAATGCATCGTCTCCGAGAGCTCTTCGCGGATTGGCAACCAATGTCGCCAACTACAACGGGTGGAACATTACCAGCCCCCCATCGTACACGCAAGGCAACGCTGTCTACAACGAGAAGCTGTACATCCACGCTATTGGACCTCTTCTTGCCAATCACGGCTGGTCCAACGCCTTCTTCATCACTGATCAAGGTCGATCGGGAAAGCAGCCTACCGGACAGCAACAGTGGGGAGACTGGTGCAATGTGATCGGCACCGGATTTGGTATTCGCCCATCCGCAAACACTGGGGACTCGTTGCTGGATTCGTTTGTCTGGGTCAAGCCAGGCGGCGAGTGTGACGGCACCAGCGACAGCAGTGCGCCACGATTTGACTCCCACTGTGCGCTCCCAGATGCCTTGCAACCGGCGCCTCAAGCTGGTGCTTGGTTCCAAGCCTACTTTGTGCAGCTTCTCACAAACGCAAACCCATCGTTCCTGTAA"
+# fill = pipeline('fill-mask', model=model, tokenizer="zhihan1996/DNABERT-2-117M", trust_remote_code=True)
+# print(transformers.__version__)
+# idx = 10
+# masked_token = sequence[idx]
+# test_seq = sequence[:idx] + fill.tokenizer.mask_token + sequence[idx + 1:]
+# # expected.append(sequence[idx])
+# print(test_seq)
+# resp = fill(test_seq)
+# print(resp)
