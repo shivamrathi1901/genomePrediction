ce6bb26ad5c27640beb3b2e97276b61443dc9209
Soumya130498 <113308735+Soumya130498@users.noreply.github.com>
2023-11-24 20:18:53 -0500

    Update test.py
    4 mer

--- GenomeBERT/test.py
+++ GenomeBERT/test.py
@@ -1,5 +1,5 @@
 
-from transformers import AutoTokenizer, AutoModel, pipeline
+from transformers import AutoTokenizer, AutoModel, pipeline, AutoModelForMaskedLM
 import random, logging, sys
 from sklearn.metrics import classification_report
 import pandas as pd
@@ -17,15 +17,18 @@ def run_test(sequences, model, tokenizer):
     result = []
     expected = []
 
-    for sequence in sequences:
-        idx = random.randint(0, len(sequence) - 1)
-        test_seq = sequence[:idx] + fill.tokenizer.mask_token + sequence[idx + 1:]
-        expected.append(sequence[idx: idx + 1])
+    for i in range(len(sequences)):
+        idx = random.randint(0, len(sequences[i]) - 1)
+        test_seq = sequences[i][:idx] + fill.tokenizer.mask_token + sequences[i][idx + 4:]
+        expected.append(sequences[i][idx: idx + 4])
+        sequences[i] = test_seq
 
-        # Directly access the model outputs without modifying the tuple
+    # Directly access the model outputs without modifying the tuple
         fill_output = fill(test_seq)
-        #resp = fill_output[0]['token_str']
-        result.append(fill_output)
+        logger.info(fill_output[0]['token_str'])
+        resp = fill_output[0]['token_str']
+    
+        result.append(fill_output[0]['token_str'])
 
     return result, expected
 
@@ -43,10 +46,10 @@ def chunk_sequences(file_path, chunk_size, model, tokenizer):
             print(classification_report(results,expected))
 
 if __name__ == '__main__':
-    csv_file_path = "./test.csv"
+    csv_file_path = "./test/Swissprot_Eukaryotes.csv"
     model_name = "GenomeBERT"
     tokenizer = AutoTokenizer.from_pretrained("zhihan1996/DNABERT-2-117M", trust_remote_code=True)
-    model = AutoModel.from_pretrained(model_name, trust_remote_code=True)
+    model = AutoModelForMaskedLM.from_pretrained(model_name, trust_remote_code=True)
     chunk_sequences(csv_file_path, chunk_size=2, model=model, tokenizer=tokenizer)
 
 # import pandas as pd
