e09e9ff238be4bb40cf2628d9834c2b8d48e0766
Ratish Jha <56468086+ratish11@users.noreply.github.com>
2023-11-25 12:00:04 -0500

    scratch tokenizer

--- GenomeBERT/train_tokenizer.py
+++ GenomeBERT/train_tokenizer.py
@@ -21,10 +21,11 @@ def get_training_corpus(sequences):
 
 def tokenize(sequences, model_name, logger):
     # not required as we are using static tokenizer
-    if(os.path.exists("models/{}".format(model_name))):
-        tokenizer = AutoTokenizer.from_pretrained("models/{}".format(model_name), trust_remote_code=True)
-    else:
-        tokenizer = AutoTokenizer.from_pretrained("zhihan1996/DNABERT-2-117M", trust_remote_code=True)
+    # if(os.path.exists("models/{}".format(model_name))):
+    #     tokenizer = AutoTokenizer.from_pretrained("models/{}".format(model_name), trust_remote_code=True)
+    # else:
+    #     tokenizer = AutoTokenizer.from_pretrained("zhihan1996/DNABERT-2-117M", trust_remote_code=True)
+    tokenizer = AutoTokenizer.from_pretrained("zhihan1996/DNABERT-2-117M", trust_remote_code=True)
     training_corpus = get_training_corpus(sequences)
     logger.info("now training tokenizer")
     tokenizer.train_new_from_iterator(training_corpus, 4096) #, special_tokens=['<s>', '<pad>', '</s>', '<unk>', '<mask>']
