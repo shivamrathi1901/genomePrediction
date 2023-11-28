9667790ddab94fcd3231bf3cf959a254b4d35991
ratish11 <ratishjha47@gmail.com>
2023-11-28 13:58:28 -0500

    flask commented for testing UI

--- Backend/app.py
+++ Backend/app.py
@@ -1,17 +1,17 @@
-import numpy as np
+
 from flask import Flask, request, jsonify
 # from sklearn.metrics import accuracy_score, f1_score, classification_report
 import  sys
-from transformers import AutoTokenizer, pipeline, AutoModelForMaskedLM
+# from transformers import AutoTokenizer, pipeline, AutoModelForMaskedLM
 
 tokenizer_name = "zhihan1996/DNABERT-2-117M"
 model_name = "models/GenomeBERT"
 
 tokenizer_name = sys.argv[1]
 model_name = sys.argv[2]
-tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, trust_remote_code=True)
-model = AutoModelForMaskedLM.from_pretrained(model_name, trust_remote_code=True)
-fill = pipeline('fill-mask', model=model, tokenizer=tokenizer)
+# tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, trust_remote_code=True)
+# model = AutoModelForMaskedLM.from_pretrained(model_name, trust_remote_code=True)
+# fill = pipeline('fill-mask', model=model, tokenizer=tokenizer)
 
 app = Flask(__name__)# Load the model
 
@@ -25,7 +25,7 @@ def predict():
     print(data)
     
     # prediction = model.predict([[np.array(data['exp'])]])    # Take the first value of prediction
-    resp = fill(data)[0]['token_str'] 
+    # resp = fill(data)[0]['token_str'] 
     return jsonify(resp)
 
 if __name__ == '__main__':
