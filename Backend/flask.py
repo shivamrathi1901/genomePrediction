902b23472617f435fe2a41cec7532b345e8b8235
Ratish Jha <56468086+ratish11@users.noreply.github.com>
2023-11-28 13:01:10 -0500

    flask

--- Backend/flask.py
+++ Backend/flask.py
@@ -2,16 +2,23 @@ import numpy as np
 from flask import Flask, request, jsonify
 from sklearn.metrics import accuracy_score, f1_score, classification_report
 import  sys, os, re, logging
-from transformers import pipeline
+from transformers import AutoTokenizer, pipeline, AutoModelForMaskedLM
+
+
+
+tokenizer = AutoTokenizer.from_pretrained("zhihan1996/DNABERT-2-117M", trust_remote_code=True)
+model = AutoModelForMaskedLM.from_pretrained("models/GenomeBERT", trust_remote_code=True)
+fill = pipeline('fill-mask', model=model, tokenizer=tokenizer)
 
 app = Flask(__name__)# Load the model
 
-fill = pipeline('fill-mask', model=model_name, tokenizer=tokenizer)
+# fill = pipeline('fill-mask', model=model_name, tokenizer=tokenizer)
 
 @app.route('/api',methods=['POST'])
 def predict():
     # Get the data from the POST request.
     data = request.get_json(force=True)    # Make prediction using model loaded from disk as per the data.
+    print(data)
     prediction = model.predict([[np.array(data['exp'])]])    # Take the first value of prediction
     resp = fill(test_seq)[0]['token_str'] 
     return jsonify(resp)
