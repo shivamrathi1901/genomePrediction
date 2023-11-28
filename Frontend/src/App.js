bfb12ac0e097eadda4de81e22fb4150650c7ac9a
Shivam Rathi <shivamrathi@gmail.com>
2023-11-28 16:11:53 -0500

    dynamic results section

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,27 +1,34 @@
 import './App.css';
 import React, { useState } from 'react';
+// Existing imports...
+
 function App() {
   const [inputText, setInputText] = useState('');
-  const [outputText, setOutputText] = useState('');
+  const [outputText, setOutputText] = useState([]);
 
   const handleChange = (event) => {
     setInputText(event.target.value);
   };
 
   const handleSubmit = async () => {
-    try {
-      const response = await fetch('http://127.0.0.1:5000/api', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ sequence: inputText }),
-      });
+    let trimmedString = inputText.trim();
+    let valuesArray = trimmedString.split(',');
+    let trimmedValuesArray = valuesArray.map((value) => value.trim());
+    for (let i = 0; i <= trimmedValuesArray.length - 1; i++) {
+      try {
+        const response = await fetch('http://127.0.0.1:5000/api', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ sequence: trimmedValuesArray[i] }),
+        });
 
-      const data = await response.json();
-      setOutputText(data);
-    } catch (error) {
-      console.error('Error calling Flask API:', error);
+        const data = await response.json();
+        setOutputText((prevOutputText) => [...prevOutputText, data]);
+      } catch (error) {
+        console.error('Error calling Flask API:', error);
+      }
     }
   };
 
@@ -40,18 +47,24 @@ function App() {
       <br />
       <div>
         <h2>OUTPUT:</h2>
-        <textarea
-          rows="4"
-          cols="50"
-          readOnly
-          value={outputText}
-          placeholder="Output will be displayed here"
-        />
-        {/* <p>{outputText}</p> */}
+        {outputText.length > 0 ? (
+          outputText.map((output, index) => (
+            <textarea
+              key={index}
+              rows="4"
+              cols="50"
+              readOnly
+              value={output}
+              placeholder="Output will be displayed here"
+            />
+          ))
+        ) : (
+          <p>No output yet</p>
+        )}
       </div>
-
     </div>
   );
 }
 
 export default App;
+
