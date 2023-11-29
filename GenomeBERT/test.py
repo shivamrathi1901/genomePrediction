19faf4dc93096e8de204990cf7df93fe055dde3b
Ratish Jha <56468086+ratish11@users.noreply.github.com>
2023-11-28 20:19:34 -0500

    test case 2

--- GenomeBERT/test.py
+++ GenomeBERT/test.py
@@ -6,7 +6,7 @@ import util
 tokenizer_dir = "zhihan1996/DNABERT-2-117M" #default tokenizer
 model_name = "zhihan1996/DNABERT-2-117M"
 model_name = sys.argv[1]
-tokenizer_dir = sys.argv[2]
+# tokenizer_dir = sys.argv[2]
 meta = "Test: Swiss trained tokenizer with Model trained till 15th epoch"
 # with open(f"{model_name}/meta.txt", 'w') as file:
 #     file.write(meta)
